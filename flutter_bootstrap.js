4a4d97053e418b66e9cd5b925f86abbf863b2e54
arifur23 <arifurrhaman9@gmail.com>
2025-10-07 05:28:01 +0600

    second commit withs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3642698575"
+    serviceWorkerVersion: "4250663922"
   }
 });
